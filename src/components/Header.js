fd2d0c4c80b4edb21048c48554c97dbd0b465608
Collid3 <masolathabangjerry11@gmail.com>
2023-09-21 09:00:35 +0200

    add image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const [selectedNav, setSelectedNav] = useState("");
   const navigate = useNavigate("");
 
-  const navTo = (destination) => {
-    console.log(destination);
+  useEffect(() => {
+    // eslint-disable-next-line default-case
+    switch (window.location.pathname) {
+      case "/":
+        setSelectedNav("home");
+        break;
 
-    navigate(`${destination}`);
-  };
+      case "/skills":
+        setSelectedNav("skills");
+        break;
+
+      case "/projects":
+        setSelectedNav("projects");
+        break;
+
+      case "/contact":
+        setSelectedNav("contact");
+        break;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [window.location.pathname]);
 
   return (
-    <header className="sticky top-0 flex justify-between items-center p-4 z-10 mb-6">
+    <header className="sticky top-0 flex justify-between items-center p-4 z-10 mb-10">
       <h1>Portfolio.</h1>
 
       <nav>
         <ul className="flex">
-          <li onClick={() => navTo("/")}>Home</li>
-          <li onClick={() => navTo("/skills")}>Skills</li>
-          <li onClick={() => navTo("/projects")}>Projects</li>
-          <li onClick={() => navTo("/contact")}>Contact</li>
+          <li
+            className={`${selectedNav === "home" ? "selected" : undefined}`}
+            onClick={() => navigate("/")}
+          >
+            Home
+          </li>
+          <li
+            className={`${selectedNav === "skills" ? "selected" : undefined}`}
+            onClick={() => navigate("/skills")}
+          >
+            Skills
+          </li>
+          <li
+            className={`${selectedNav === "projects" ? "selected" : undefined}`}
+            onClick={() => navigate("/projects")}
+          >
+            Projects
+          </li>
+          <li
+            className={`${selectedNav === "contact" ? "selected" : undefined}`}
+            onClick={() => navigate("/contact")}
+          >
+            Contact
+          </li>
         </ul>
       </nav>
     </header>
--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -1,26 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
+  const navigate = useNavigate("");
+
   return (
-    <div className="hero-container h-full flex flex-col items-center justify-center gap-7">
-      <h1 className="text-6xl">
-        <span className="font-normal text-4xl">HI, I AM</span> JERRY MASOLA
-      </h1>
-      <h2 className="text-4xl">5 YEARS FULL STACK DEVELOPER</h2>
+    <div className="main-hero flex justify-between items-center h-full">
+      <div className="hero-container flex flex-col items-center justify-center gap-7">
+        <h1 className="text-6xl">
+          <span className="font-normal text-4xl">HI, I AM</span> JERRY MASOLA
+        </h1>
+        <h2 className="text-4xl">5 YEARS FULL STACK DEVELOPER</h2>
 
-      <p>
-        Hardworking and passionate Self-driven developer dedicated to developing
-        user-friendly and feature-rich websites with strong organization.
-        Proficient in developing servers, user interfaces, testing and
-        debugging. Dependable developer successful at managing multiple
-        priorities with a positive attitude. Willingness to take on added
-        responsibilities to meet team goals.
-      </p>
+        <p>
+          Hardworking and passionate Self-driven developer dedicated to
+          developing user-friendly and feature-rich websites with strong
+          organization. Proficient in developing servers, user interfaces,
+          testing and debugging. Dependable developer successful at managing
+          multiple priorities with a positive attitude. Willingness to take on
+          added responsibilities to meet team goals.
+        </p>
 
-      <section className="flex gap-5">
-        <button className="px-8 py-2">PROJECTS</button>
-        <button className=" px-8 py-2">CONTACT</button>
-      </section>
+        <section className="flex gap-5">
+          <button className="px-8 py-2" onClick={() => navigate("/projects")}>
+            PROJECTS
+          </button>
+          <button className=" px-8 py-2" onClick={() => navigate("/contact")}>
+            CONTACT
+          </button>
+        </section>
+      </div>
+
+      <img src={require("../images/myself.jpg")} alt="" />
     </div>
   );
 };
